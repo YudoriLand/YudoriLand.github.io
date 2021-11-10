ffbfddd7ebecdbbf7a507d95ec40df7735013fd6
eukkdoli <browndust170424@gmail.com>
2021-11-10 20:48:09 +0900

    20211110 Upload

---
+++ js/yuri_run.js
@@ -0,0 +1,16 @@
+//var canvas = document.getElementById('canvas');
+//var ctx = canvas.getContext;
+//
+//if (canvas.getContext) {
+//    var ctx = canvas.getContext("2d");
+//
+//} else {
+//    console.log("no canvas");
+//}
+//
+//var img = new Image();
+//img.src = '/assets/images/yuri_svg/yuri_run_1.svg';
+//
+//img.onload = function () {
+//    ctx.drawImage(img, 0, 0, 50, 50);
+//}
