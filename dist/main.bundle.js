f576e2f53d86da0dc6ae2dcfe0ccdf40528d87d5
eukkdoli <browndust170424@gmail.com>
2021-11-25 11:20:10 +0900

    test commit

---
+++ dist/main.bundle.js
@@ -0,0 +1,52 @@
+(self["webpackChunkYudori_s_Land"] = self["webpackChunkYudori_s_Land"] || []).push([["main"],{
+
+/***/ "./src/resources/style.css":
+/*!*********************************!*\
+  !*** ./src/resources/style.css ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _resources_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./resources/style.css */ "./src/resources/style.css");
+
+
+/***/ }),
+
+/***/ "./src/resources/js/cursor.js":
+/*!************************************!*\
+  !*** ./src/resources/js/cursor.js ***!
+  \************************************/
+/***/ (() => {
+
+var cursor = document.getElementById('cursor');
+
+document.addEventListener("mousemove", (e) => {
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    cursor.style.left = mouseX + "px";
+    cursor.style.top = mouseY + "px";
+});
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/main.js"), __webpack_exec__("./src/resources/js/cursor.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUVBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ZdWRvcmkncyBMYW5kLy4vc3JjL3Jlc291cmNlcy9zdHlsZS5jc3M/YWY4YyIsIndlYnBhY2s6Ly9ZdWRvcmkncyBMYW5kLy4vc3JjL21haW4uanMiLCJ3ZWJwYWNrOi8vWXVkb3JpJ3MgTGFuZC8uL3NyYy9yZXNvdXJjZXMvanMvY3Vyc29yLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpblxuZXhwb3J0IHt9OyIsImltcG9ydCAnLi9yZXNvdXJjZXMvc3R5bGUuY3NzJzsiLCJ2YXIgY3Vyc29yID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ2N1cnNvcicpO1xyXG5cclxuZG9jdW1lbnQuYWRkRXZlbnRMaXN0ZW5lcihcIm1vdXNlbW92ZVwiLCAoZSkgPT4ge1xyXG4gICAgbGV0IG1vdXNlWCA9IGUuY2xpZW50WDtcclxuICAgIGxldCBtb3VzZVkgPSBlLmNsaWVudFk7XHJcbiAgICBjdXJzb3Iuc3R5bGUubGVmdCA9IG1vdXNlWCArIFwicHhcIjtcclxuICAgIGN1cnNvci5zdHlsZS50b3AgPSBtb3VzZVkgKyBcInB4XCI7XHJcbn0pOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
